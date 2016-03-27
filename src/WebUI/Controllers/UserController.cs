3ff34c7e2aa76e9932ac110b31fd83cf5706679f
Mrinal Saurabh <msaurabh92@gmail.com>
2016-03-27 19:23:16 +0530

    Added Master Layout

---
+++ src/WebUI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+    }
+}
